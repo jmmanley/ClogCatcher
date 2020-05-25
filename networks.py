fix softmax bug

networks.py
@@ -9,8 +9,10 @@ from tqdm import tqdm
 
 import tensorflow as tf
 from tensorflow.keras.layers import Input, Dropout, Bidirectional, LSTM, Dense
+from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 
 class CNNFeatureLSTM:
@@ -32,7 +34,8 @@ class CNNFeatureLSTM:
 	.summary(), .fit(), .predict() designed to feel like Keras.Model methods.
 	"""
 
-	def __init__(self, cnn, cnn_dim=1000, dropout=0.1, padframes=100, *args, **kwargs):
+	def __init__(self, cnn, cnn_dim=1000, dropout=0.1, padframes=100, 
+		         lstm_units=64, *args, **kwargs):
 
 		self.cnn = cnn
 		self.cnn_dim = cnn_dim
@@ -41,27 +44,27 @@ class CNNFeatureLSTM:
 		# DEFINE MANY-TO-ONE LSTM MODEL FOR CLASSIFICATION
 		self.model = tf.keras.Sequential()
 		self.model.add(Input(shape=(padframes,cnn_dim)))
-		self.model.add(Bidirectional(LSTM(cnn_dim)))
+		self.model.add(Bidirectional(LSTM(lstm_units)))
 		self.model.add(Dropout(dropout))
 		self.model.add(Dense(64,      activation='relu'))
-		self.model.add(Dense(1,       activation='softmax'))
+		self.model.add(Dense(1,       activation='sigmoid'))
 
 		self.model.compile(loss='binary_crossentropy',
 						   optimizer='adam',
-						   metrics=['accuracy'], # try something else?
+						   metrics=['accuracy'],
+						   callbacks=[EarlyStopping(monitor='val_loss', patience=5)],
 						   *args, **kwargs) 
 
 
 	def summary(self):
-		print('CNN Model:', self.cnn._keras_api_names)
+		print('CNN Model:', self.cnn._name)
 		self.model.summary()
 
 
 	def fit(self, clogData, epochs=10, validate=0.2, **kwargs):
 
 		# LOAD DATA AND PUT THROUGH CNN
-		train_file = os.path.join(clogData.path, 
-			                      self.cnn._name[-1].replace('.','_') + '_features.npy')
+		train_file = os.path.join(clogData.path, self.cnn._name + '_features.npy')
 
 		if os.path.exists(train_file):
 			print('LOADING CNN FEATURES...')
@@ -77,9 +80,12 @@ class CNNFeatureLSTM:
 				vid = clogData.load(clogData.train.index[i], train=True)
 				cnn_features = self.cnn.predict(vid)
 
-				train_data[i] = pad_sequences(cnn_features.T, maxlen=self.padframes).T
+				train_data[i] = pad_sequences(cnn_features.T, maxlen=self.padframes, dtype='float').T
 
 			np.save(train_file, train_data)
+
+		for i in range(train_data.shape[2]):
+			train_data[:,:,i] = StandardScaler().fit_transform(train_data[:,:,i])
 
 		train_labels = np.asarray(clogData.train.stalled.values)
 